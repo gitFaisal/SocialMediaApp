7a5345debcfdd950fd3971cf3372cb17d3abaec4
Faisal Malik <50165811+gitFaisal@users.noreply.github.com>
2020-01-21 12:17:26 -0500

    Add files via upload

---
+++ mysite/blog/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Post, Comment
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
